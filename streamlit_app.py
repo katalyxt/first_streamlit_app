904a02df2daf570564aaf80c12590048fc36ecbb
katalyxt <93821569+katalyxt@users.noreply.github.com>
2023-01-03 16:02:21 +0530

    Update streamlit_app.py
    Added fruit list for Build your own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+
+import pandas
+
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
@@ -7,3 +10,9 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-BOiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
+
+
